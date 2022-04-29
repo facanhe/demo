31d6d7d31c953e51a7b33772a9646ce2c58ecfed
facanhe <1841634171@qq.com>
2022-04-29 16:33:55 +0800

    429butval

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },{
+    path: '/index',
+    name: 'index',
+    component: Index
+  },
 ]
 
 const router = createRouter({
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>
-        Index
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item label="Approved by">
+            <el-input v-model="formInline.user" placeholder="Approved by" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit(formInline.user)">Query</el-button>
+          </el-form-item>
+        </el-form>
+      </el-header>
+      <el-main>Main{{formInline.user}}</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
+<script setup>
+import { reactive } from 'vue'
+
+const formInline = reactive({
+  user: '',
+})
+
+const onSubmit = (parm) => {
+  console.log('submit!')
+  console.log(parm)
+}
+</script>
