32d5d129554ce5934ffa73633d1e994033e3a43f
facanhe <1841634171@qq.com>
2022-05-05 16:21:57 +0800

    55axios数据展示

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,24 +7,40 @@
             <el-input v-model="formInline.user" placeholder="Approved by" />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit(formInline.user)">Query</el-button>
+            <el-button type="primary" @click="onSubmit(formInline.user)"
+              >Query</el-button
+            >
           </el-form-item>
         </el-form>
       </el-header>
-      <el-main>Main{{formInline.user}}</el-main>
+      <el-main>
+        <el-table :data="tableData" stripe style="width: 100%">
+          <el-table-column prop="id" label="学号" width="180" />
+          <el-table-column prop="name" label="姓名" width="180" />
+          <el-table-column prop="age" label="年龄" />
+          <el-table-column prop="hoppy" label="爱好" />
+        </el-table>
+      </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </div>
 </template>
 <script setup>
-import { reactive } from 'vue'
-
+import { reactive,ref } from "vue";
+import axios from "axios";
 const formInline = reactive({
-  user: '',
-})
-
+  user: "",
+});
+const tableData=ref();
+axios.get('http://localhost:8080/student/getById').then((response)=>{
+    tableData.value=response.data
+    console.log(response.data)
+    console.log(tableData)
+  })
 const onSubmit = (parm) => {
-  console.log('submit!')
-  console.log(parm)
-}
+  
+  console.log("submit!");
+  console.log(parm);
+  
+};
 </script>
