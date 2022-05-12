203d4e27692e196d00d353e1ec3d846fe6ce093e
facanhe <1841634171@qq.com>
2022-05-12 15:35:58 +0800

    512form

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,13 +3,11 @@
     <el-container>
       <el-header>
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
-          <el-form-item label="Approved by">
-            <el-input v-model="formInline.user" placeholder="Approved by" />
+          <el-form-item label="请输入学号">
+            <el-input v-model="formInline.user" placeholder="学号" />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit(formInline.user)"
-              >Query</el-button
-            >
+            <el-button type="primary" @click="onSubmit(formInline.user)">查询</el-button>
           </el-form-item>
         </el-form>
       </el-header>
@@ -19,6 +17,12 @@
           <el-table-column prop="name" label="姓名" width="180" />
           <el-table-column prop="age" label="年龄" />
           <el-table-column prop="hoppy" label="爱好" />
+          <el-table-column fixed="right" label="操作" width="120">
+            <template #default>
+              <el-button type="text" size="small" @click="handleClick">编辑</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
         </el-table>
       </el-main>
       <el-footer>Footer</el-footer>
@@ -26,21 +30,40 @@
   </div>
 </template>
 <script setup>
-import { reactive,ref } from "vue";
+import { reactive, ref } from "vue";
 import axios from "axios";
+import { ElMessage } from "element-plus";
 const formInline = reactive({
   user: "",
 });
-const tableData=ref();
-axios.get('http://localhost:8080/student/getById').then((response)=>{
-    tableData.value=response.data
-    console.log(response.data)
-    console.log(tableData)
+const tableData = ref();
+
+axios.get("http://localhost:8080/student/getById")
+  .then((response) => {
+    tableData.value = response.data;
+    console.log(response.data);
+    console.log(tableData);
+    ElMessage({
+      message: "Congrats, this is a success message.",
+      type: "success",
+    });
   })
+  .catch(function (error) {
+    ElMessage.error("获取数据失败");
+  });
 const onSubmit = (parm) => {
-  
   console.log("submit!");
   console.log(parm);
+  axios.get("http://localhost:8080/student/findById/"+parm).then((response)=>{
+    tableData.value=response.data;
+    console.log(tableData)
+    ElMessage({
+    message: "查询成功！",
+    type: "success",
+  })
+  }).catch(function (error) {
+    ElMessage.error("获取数据失败");
+  });
   
 };
 </script>
